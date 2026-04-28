5ba4f96b4adb7e193d70ba5fc5b581096dc36990
Yuriy Silvestrov <ysilvestrov.aggregator@gmail.com>
2026-04-28 22:28:05 +0000

    feat(bot): wire i18n middleware into BotContext + Telegraf chain

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,6 +2,8 @@ import { Telegraf, Context } from 'telegraf';
 import type pino from 'pino';
 import type { DB } from '../storage/db';
 import type { Env } from '../config/env';
+import type { Locale, Translator } from '../i18n/types';
+import { i18nMiddleware } from './middleware/i18n';
 
 export interface AppDeps {
   db: DB;
@@ -11,6 +13,8 @@ export interface AppDeps {
 
 export interface BotContext extends Context {
   deps: AppDeps;
+  locale: Locale;
+  t: Translator;
 }
 
 export function createBot(deps: AppDeps): Telegraf<BotContext> {
@@ -19,6 +23,7 @@ export function createBot(deps: AppDeps): Telegraf<BotContext> {
     ctx.deps = deps;
     return next();
   });
+  bot.use(i18nMiddleware);
   bot.catch((err, ctx) => deps.log.error({ err, update: ctx.update }, 'bot error'));
   return bot;
 }
